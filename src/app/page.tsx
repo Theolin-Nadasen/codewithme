40c71f9c0007c4788fc88baa3c5bd721938f4c00
Theolin Nadasen <nadasentheolin@gmail.com>
2025-07-16 14:25:24 +0200

    added links to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import Github_Button from "@/components/github_button";
 import Main_Hero_Section from "@/components/main_hero_section";
+import Link from "next/link";
+import Youtube_Button from "@/components/youtube_button";
 
 export default function Home() {
 
   return (
     <div>
       <Main_Hero_Section />
-      <h1>Theo</h1>
+
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 mt-5">
+        <Link href={"https://github.com/Theolin-Nadasen"}>
+          <Github_Button />
+        </Link>
+
+        <Link href={"https://www.youtube.com/@codewithme-theo"}>
+          <Youtube_Button />
+        </Link>
+      </div>
     </div>
   );
 }
