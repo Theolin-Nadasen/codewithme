cf8d5070e99fb843ff02a57523d3d40cd4e9f311
Theolin Nadasen <nadasentheolin@gmail.com>
2025-07-16 12:50:53 +0200

    animated text on hero

--- src/components/main_hero_section.tsx
+++ src/components/main_hero_section.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { TypeAnimation } from "react-type-animation"
+
 export default function Main_Hero_Section() {
     return (
         <>
@@ -5,13 +8,45 @@ export default function Main_Hero_Section() {
 
                 <h1 className="text-5xl font-extrabold text-green-500">CODE WITH ME</h1>
 
-                <h1>Some text that says some stuff</h1>
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'A developer building engaging digital worlds through websites',
+                        1000, // wait 1s before replacing 
+                        'A developer building engaging digital worlds through apps',
+                        1000,
+                        'A developer building engaging digital worlds through games',
+                        1000,
+                        'A developer building engaging digital worlds through digital content',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: '2em', display: 'inline-block' }}
+                    repeat={Infinity}
+                />
 
             </div>
 
             <div className="flex md:hidden h-32 bg-gray-800 opacity-70 items-center justify-center">
 
-                <h1>Mobile text that says something</h1>
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'A developer building engaging digital worlds through websites',
+                        1000, // wait 1s before replacing 
+                        'A developer building engaging digital worlds through apps',
+                        1000,
+                        'A developer building engaging digital worlds through games',
+                        1000,
+                        'A developer building engaging digital worlds through digital content',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: '1em', display: 'inline-block', padding: '50px' }}
+                    repeat={Infinity}
+                />
 
             </div>
         </>
