e058e7bdae59fc7a41d3d74d969e702a17f976ee
Theolin Nadasen <nadasentheolin@gmail.com>
2025-07-16 19:18:24 +0200

    fixed margin and padding on footer

--- src/components/main_footer_bar.tsx
+++ src/components/main_footer_bar.tsx
@@ -1,6 +1,6 @@
 export default function Main_Footer_bar(){
     return(
-        <footer className="flex justify-center opacity-30 md:opacity-50 bg-black">
+        <footer className="flex justify-center opacity-30 md:opacity-50 bg-gray-800 mt-5 py-5">
             <p className="mb-2">© {new Date().getFullYear()} Theolin Nadasen. All Rights Reserved.</p>
         </footer>
     )
