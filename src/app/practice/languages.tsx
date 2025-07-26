c65ebc03bd152a27d453203c40444c4035a6f392
Theolin Nadasen <nadasentheolin@gmail.com>
2025-07-27 01:16:06 +0200

    added typescript and c

--- src/app/practice/languages.tsx
+++ src/app/practice/languages.tsx
@@ -6,5 +6,13 @@ export const Languages = [
     {
         name : "javascript",
         version : "18.15.0"
-    }
+    },
+    {
+        name : "typescript",
+        version : "1.32.3"
+    },
+    {
+        name : "c",
+        version : "10.2.0"
+    },
 ]
