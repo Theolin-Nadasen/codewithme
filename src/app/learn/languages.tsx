3acb0f1f6945ffb1f5583d7b19c648065811f065
Theolin Nadasen <nadasentheolin@gmail.com>
2025-07-27 13:16:35 +0200

    added support for inputs

--- src/app/learn/languages.tsx
+++ src/app/learn/languages.tsx
@@ -1,15 +1,15 @@
 export const Languages = [
     {
-        name : "python",
-        version : "3.10.0"
-    },
-    {
         name : "javascript",
         version : "18.15.0"
     },
     {
         name : "typescript",
         version : "1.32.3"
+    },
+    {
+        name : "python",
+        version : "3.10.0"
     },
     {
         name : "c",
--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -5,12 +5,14 @@ import axios from "axios";
 import { Languages } from "./languages";
 
 export default function CodeRunner() {
-  const [code, setCode] = useState(`print("Hello world!")`);
+  const [code, setCode] = useState(`console.log("Code With Me")`);
   const [selectedLanguage, setSelectedLanguage] = useState(Languages[0]);
   const [output, setOutput] = useState("");
 
   const buttonRef = useRef<HTMLButtonElement>(null);
   const [isRunning, setIsRunning] = useState(false);
+
+  const [inputs, setInputs] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const lang = Languages.find(l => l.name === e.target.value);
@@ -23,6 +25,8 @@ export default function CodeRunner() {
     setIsRunning(true)
     buttonRef.current.disabled = true;
 
+    console.log(`inputs: ${inputs}`);
+
     try {
       const res = await axios.post("https://emkc.org/api/v2/piston/execute/", {
         language: selectedLanguage.name,
@@ -30,7 +34,8 @@ export default function CodeRunner() {
         files: [{
           name: "test.js",
           content: code,
-        }]
+        }],
+        stdin: inputs.replaceAll("\\n", "\n")
       });
 
       setOutput(res.data.run.stdout);
@@ -45,6 +50,11 @@ export default function CodeRunner() {
     setIsRunning(false)
     buttonRef.current.disabled = false;
   };
+
+  const addInput = () => {
+    setInputs(inputs + "\\n");
+    console.log(inputs);
+  }
 
   return (
     <div className="p-5 space-y-5">
@@ -62,6 +72,14 @@ export default function CodeRunner() {
         ))}
       </select>
 
+      <button
+        ref={buttonRef}
+        onClick={runCode}
+        className={`px-4 py-2 mx-2 bg-green-600 text-white rounded ${isRunning ? "opacity-50" : " "}`}
+      >
+        {isRunning ? "running..." : "run"}
+      </button>
+
       <div className="rounded-2xl overflow-hidden shadow-lg shadow-green-400 border-4 border-amber-200">
         <Editor
           height="300px"
@@ -73,13 +91,9 @@ export default function CodeRunner() {
         />
       </div>
 
-      <button
-        ref={buttonRef}
-        onClick={runCode}
-        className={`px-4 py-2 bg-green-600 text-white rounded ${isRunning ? "opacity-50" : " "}`}
-      >
-        {isRunning ? "running..." : "run"}
-      </button>
+      <label className="block mb-2 text-sm font-medium text-white">Inputs:</label>
+      <input type="text" className="bg-black p-2 font-bold" value={inputs} onChange={(e) => setInputs(e.currentTarget.value)}/>
+      <button onClick={addInput}>➕</button>
 
       <pre className="bg-black text-white p-3 rounded-2xl">
         {output || "Output will appear here..."}
